7b036a15a99e0c28b1131743ff6774a866bedc9a
Michael Odjidja <82180865+thekidjnr@users.noreply.github.com>
2024-12-28 19:05:24 +0000

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -41,7 +41,7 @@ app.use(cookieParser());
 
 // CORS POLICIES
 const corsOptions: CorsOptions = {
-  origin: ["https://lenslyst.com", "http://localhost:3000"],
+  origin: ["https://www.lenslyst.com", "http://localhost:3000"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true,
   exposedHeaders: ["Set-Cookie", "Date", "ETag"],
